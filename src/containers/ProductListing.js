246c8c7c47b908fbcebb053d3c3670548715a8f6
Ashrafweb <76968589+Ashrafweb@users.noreply.github.com>
2021-07-03 23:32:08 +0600

    Update ProductListing.js

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,21 +1,10 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import ProductComponent from "./ProductComponent";
 import { fetchProducts } from "../redux/actions/productActions";
 
 function ProductListing() {
-  const products = useSelector((state) => state);
   const dispatch = useDispatch();
-
-  // const fetchproducts = async () => {
-  //   const response = await axios
-  //     .get("https://fakestoreapi.com/products")
-  //     .catch((err) => {
-  //       console.log("Err", err);
-  //     });
-  //   console.log(response.data);
-  //   dispatch(setProducts(response.data));
-  // };
 
   useEffect(() => {
     dispatch(fetchProducts());
